9e05ef1cec047745f20cfa841caecd0e5b3eed53
nehara1 <142091393+nehara1@users.noreply.github.com>
2023-09-11 14:27:46 +0530

    scrool
    image

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let mynav = document.querySelector('.navbar');
 
 
 
-menubar.onclick = () =>{
+menubar.onclick = () => {
     menubar.classList.toggle('fa-times')
     mynav.classList.toggle('active')
 }
@@ -11,15 +11,15 @@ menubar.onclick = () =>{
 // read more
 
 
-const parentContainer =  document.querySelector('.read-more-container');
+const parentContainer = document.querySelector('.read-more-container');
 
-parentContainer.addEventListener('click', event=>{
+parentContainer.addEventListener('click', event => {
 
     const current = event.target;
 
     const isReadMoreBtn = current.className.includes('read-more-btn');
 
-    if(!isReadMoreBtn) return;
+    if (!isReadMoreBtn) return;
 
     const currentText = event.target.parentNode.querySelector('.read-more-text');
 
@@ -28,4 +28,38 @@ parentContainer.addEventListener('click', event=>{
     current.textContent = current.textContent.includes('Read More') ? "Read Less..." : "Read More...";
 
 })
+
+
+// image scroll 
+
+// Function to automatically switch between slides
+function autoScroll() {
+    // Get all the radio buttons
+    const radioButtons = document.querySelectorAll('input[name="r"]');
+    let currentIndex = 0;
+
+    // Function to show the next slide
+    function showNextSlide() {
+        radioButtons[currentIndex].checked = false;
+        currentIndex = (currentIndex + 1) % radioButtons.length;
+        radioButtons[currentIndex].checked = true;
+    }
+
+    // Auto-scroll interval (change the time interval as needed)
+    const interval = setInterval(showNextSlide, 5000); // Change the time (in milliseconds)
+
+    // Stop auto-scrolling when the user interacts with the slider
+    document.querySelector('#slider').addEventListener('mouseover', () => {
+        clearInterval(interval);
+    });
+
+    // Resume auto-scrolling when the user stops interacting with the slider
+    document.querySelector('#slider').addEventListener('mouseout', () => {
+        interval = setInterval(showNextSlide, 5000); // Change the time (in milliseconds)
+    });
+}
+
+// Call the autoScroll function when the page loads
+window.onload = autoScroll;
+
 
